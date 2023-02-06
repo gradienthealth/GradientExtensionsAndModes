16e0d2e267536372d4ddd0b17225671a0322a8b7
Ouwen Huang <ouwen.huang@duke.edu>
2023-02-06 10:59:06 +0000

    feat: simplified cache

--- extensions/ohif-gradienthealth-extension/src/services/CacheAPIService/CacheAPIService.ts
+++ extensions/ohif-gradienthealth-extension/src/services/CacheAPIService/CacheAPIService.ts
@@ -1,17 +1,14 @@
-import { pubSubServiceInterface } from '@ohif/core';
-// TODO this will be replaced by a TypeScript version in about 1 month
+import { DicomMetadataStore, pubSubServiceInterface } from '@ohif/core';
 import { internal } from 'cornerstone-wado-image-loader';
-import _cloneDeep from 'lodash.clonedeep';
+const { getOptions } = internal;
 import _ from 'lodash';
-
 import {
   eventTarget,
   EVENTS,
   Enums,
-  imageRetrievalPoolManager,
+  imageLoader,
+  imageLoadPoolManager,
 } from '@cornerstonejs/core';
-
-const { getOptions, xhrRequest } = internal;
 
 const LOCAL_EVENTS = {};
 
@@ -23,13 +20,17 @@ export default class CacheAPIService {
   private extensionManager;
   private dataSource;
   private options;
+  public storageUsage;
+  public storageQuota;
 
   constructor(servicesManager, commandsManager, extensionManager) {
-    Object.assign(this, pubSubServiceInterface);
     this.listeners = {};
     this.EVENTS = LOCAL_EVENTS;
     this.commandsManager = commandsManager;
     this.extensionManager = extensionManager;
+    this.storageUsage = null;
+    this.storageQuota = null;
+    Object.assign(this, pubSubServiceInterface);
   }
 
   public init() {
@@ -37,15 +38,6 @@ export default class CacheAPIService {
     const dataSources = this.extensionManager.getActiveDataSource();
     this.dataSource = dataSources[0];
 
-    // Need to add event listener if the configuration changes
-    this.options = Object.assign(_cloneDeep(getOptions()), {
-      writeCache: true,
-    });
-
-    eventTarget.addEventListener(
-      'CORNERSTONE_LOCALFORAGE_WRITE_FAILURE',
-      this.handleLocalForageWriteError.bind(this)
-    );
     eventTarget.addEventListener(
       'CORNERSTONE_CACHE_QUOTA_EXCEEDED_ERROR',
       this.handleQuotaExceededWriteError.bind(this)
@@ -60,6 +52,8 @@ export default class CacheAPIService {
           estimate.quota * 1e-9,
           ' GB'
         );
+        this.storageUsage = estimate.usage;
+        this.storageQuota = estimate.quota;
       });
     }
 
@@ -89,168 +83,132 @@ export default class CacheAPIService {
     }
   }
 
-  public async getUrlsForStudyInstanceUID(StudyInstanceUID) {
-    const series = await this.dataSource.getImageIdsForStudy(
-      StudyInstanceUID
+  public async setViewedStudy(StudyInstanceUID) {
+    await this.dataSource.retrieve.series.metadata({ StudyInstanceUID });
+    const study = DicomMetadataStore.getStudy(StudyInstanceUID);
+    const imageIds = study.series.flatMap((serie) =>
+      serie.instances.flatMap((instance) => instance.imageId)
     );
-
-    return _.flatten(series.map(s =>
-      s.map(imageId => imageId.split(':').slice(1).join(':'))
-    ));
-  }
-
-  public async getUrlsForStudyInstanceUIDs(StudyInstanceUIDs) {
-    return _.flatten(await Promise.all(StudyInstanceUIDs.map(this.getUrlsForStudyInstanceUID.bind(this))))
-  }
-
-  public requestAndCacheUrl(url){
-    const imageId = 'wadors:' + url
-    imageRetrievalPoolManager.addRequest(
-      xhrRequest.bind(
-        null,
-        url,
-        imageId,
-        {
-          Accept: 'application/octet-stream',
-        },
-        {},
-        this.options
-      ),
-      Enums.RequestType.Prefetch,
-      { imageId },
-      0
+    const urls = imageIds.map((imageId) =>
+      imageId.split(':').slice(1).join(':')
     );
-  }
-
-  public requestAndCacheUrls(urls){
-    const getScope = this.options.cache.getScope;
-    const keys = _.uniq(urls.map((url)=> getScope({ url })))
-    console.log('adding series to cache ', keys);
-    urls.forEach(url => this.requestAndCacheUrl(url))
-  }
-
-  public async removeOldStudies(staleTime = 1 * 24 * 60 * 60 * 1000) {
-    console.warn(
-      'Removing studies older than ',
-      staleTime / 1000 / 60 / 60 / 24,
-      ' days'
-    );
-    const keys = await window?.localforage.keys();
-    const values = await Promise.all(
-      keys.map((key) => window?.localforage.getItem(key))
-    );
-    const zip = (a, b) => a.map((k, i) => [k, b[i]]);
-    const keyValuePair = zip(keys, values);
-    const oldStudies = keyValuePair.filter((ele) => {
-      const [key, value] = ele;
-      const ms = Date.now() - value.date;
-      if (ms > staleTime) {
-        return true;
-      } else {
-        return false;
-      }
+    const options = getOptions();
+    const getScope = options.cache.getScope;
+    const scopes = _.uniq(urls.map((url) => getScope({ url })));
+    scopes.forEach(async (scope) => {
+      const cache = await caches.open(scope);
+      const keys = await cache.keys();
+      keys.forEach(async (key) => {
+        const res = await cache.match(key.url, {
+          ignoreVary: true,
+          ignoreMethod: true,
+          ignoreSearch: true,
+        });
+        if(!res) return
+        const req = new Request(key.url, {
+          headers: {
+            'dicom-last-put-date': new Date().toUTCString(),
+            'dicom-last-viewed-date': new Date().toUTCString(),
+            'dicom-content-length': key.headers.get('dicom-content-length')
+          },
+        });
+        if (res) {
+          cache.put(req, res);
+        }
+      });
     });
-
-    const promises = [];
-    oldStudies.forEach((ele) => {
-      const [key, value] = ele;
-      promises.push(window?.caches.delete(key));
-      promises.push(window?.localforage.removeItem(key));
-    });
-    return Promise.all(promises);
   }
 
-  public async cacheStudyImageId(StudyInstanceUID) {
-    const seriesImageIds = await this.dataSource.getImageIdsForStudy(
-      StudyInstanceUID
+  public async cacheStudy(StudyInstanceUID) {
+    await this.dataSource.retrieve.series.metadata({ StudyInstanceUID });
+    const study = DicomMetadataStore.getStudy(StudyInstanceUID);
+    const imageIds = study.series.flatMap((serie) =>
+      serie.instances.flatMap((instance) => instance.imageId)
     );
-    seriesImageIds.map((imageIds) =>
-      imageIds.map((imageId) => {
-        const url = imageId.split(':').slice(1).join(':');
-        imageRetrievalPoolManager.addRequest(
-          xhrRequest.bind(
-            null,
-            url,
-            imageId,
-            {
-              Accept: 'application/octet-stream',
-            },
-            {},
-            this.options
-          ),
-          Enums.RequestType.Background,
-          { imageId },
-          0
-        );
-      })
-    );
+    this.cacheImageIds(imageIds);
+  }
+
+  public async cacheSeries(StudyInstanceUID, SeriesInstanceUID) {
+    await this.dataSource.retrieve.series.metadata({ StudyInstanceUID });
+    const study = DicomMetadataStore.getStudy(StudyInstanceUID);
+    const imageIds = study.series
+      .filter((serie) => serie.SeriesInstanceUID === SeriesInstanceUID)
+      .flatMap((serie) =>
+        serie.instances.flatMap((instance) => instance.imageId)
+      );
+    this.cacheImageIds(imageIds);
+  }
+
+  public cacheImageIds(imageIds) {
+    function sendRequest(imageId, options) {
+      return imageLoader.loadAndCacheImage(imageId, options).then(
+        () => {},
+        (error) => {
+          console.error(error);
+        }
+      );
+    }
+
+    const priority = 0;
+    const requestType = Enums.RequestType.Prefetch;
+    const options = {
+      preScale: {
+        enabled: true,
+      },
+      useRGBA: true,
+    };
+
+    imageIds.forEach((imageId) => {
+      const additionalDetails = { imageId };
+      imageLoadPoolManager.addRequest(
+        sendRequest.bind(this, imageId, options),
+        requestType,
+        additionalDetails,
+        priority
+      );
+    });
   }
 
   public async cacheMissingStudyImageIds(StudyInstanceUIDs) {
-    const urls = await this.getUrlsForStudyInstanceUIDs(StudyInstanceUIDs);
-    const getScope = this.options.cache.getScope;
-    const isEqual = (a,b)=>{
-      return getScope({url: a}) == b
-    }
     const existingKeys = await window.caches.keys();
-    this.requestAndCacheUrls(_.differenceWith(urls, existingKeys, isEqual));
-  }
-
-  public async removeCacheUrls(urls) {
-    const getScope = this.options.cache.getScope;
-    const keys = _.uniq(urls.map((url)=> getScope({ url })))
-    console.log('delete series from cache ', keys);
-    const promises = []
-    keys.forEach(key=>{
-      promises.push(window?.caches.delete(key));
-      promises.push(window?.localforage.removeItem(key));
-    })
-    return Promise.all(promises);
-  }
-
-  public async removeStudyImageId(StudyInstanceUID) {
-    const urls = await this.getUrlsForStudyInstanceUID(StudyInstanceUID)
-    return this.removeCacheUrls(urls)
-  }
-
-  public async removeStudyImageIds(StudyInstanceUIDs) {
-    const urls = await this.getUrlsForStudyInstanceUIDs(StudyInstanceUIDs)
-    return this.removeCacheUrls(urls)
-  }
-
-  private async handleLocalForageWriteError(evt) {
-    const { error, localforage, scopes } = evt.detail;
-    console.warn(error);
-    const existence = await scopes.map((scope) => localforage.getItem(scope)); // get keys which are partially saved
-    const partiallySavedKeys = scopes.filter((ele, idx) => {
-      if (existence[idx]) return true;
-      return false;
+    const existingStudyInstanceUIDs = existingKeys.map(
+      (key) => key.split('studies/')[1].split('/')[0]
+    );
+    const newStudies = StudyInstanceUIDs.filter(
+      (studyuid) => existingStudyInstanceUIDs.indexOf(studyuid) === -1
+    );
+    newStudies.forEach((studyuid) => {
+      this.cacheStudy(studyuid);
     });
-
-    const localForagePromises = partiallySavedKeys.map((scope) =>
-      localforage.removeItem(scope)
-    );
-    const cacheAPIPromises = partiallySavedKeys.map((scope) =>
-      window.caches.delete(scope)
-    );
-    return Promise.all([...localForagePromises, ...cacheAPIPromises]);
   }
 
+  /**
+   * Removes all cached images which have been seen.
+   * @param evt
+   * @returns
+   */
   private async handleQuotaExceededWriteError(evt) {
-    const { error, localforage, scope, cachePoolAPIManager, retry } =
-      evt.detail;
-    imageRetrievalPoolManager.clearRequestStack(Enums.RequestType.Background);
-    await this.removeOldStudies();
-    await window.caches.delete(scope); // delete any partially completed cache
-    await cachePoolAPIManager.saveLocalForagePool(); // save the rest of the localforage pool
-    return localforage.removeItem(scope); // remove the partially saved name scope
+    const scopes = await window.caches.keys();
+    scopes.forEach(async (scope) => {
+      try {
+        const cache = await caches.open(scope);
+        const k = await cache.keys();
+        const lastViewed = k[0].headers.get('dicom-last-viewed-date');
+        const utctime = k[0].headers.get('dicom-last-put-date');
+        const date = utctime ? new Date(utctime) : new Date();
+        const timeNow = new Date();
+        const timeSincePut = timeNow - date;
+        const millisecondsInDay = 8.64e7;
+        if (lastViewed !== 'undefined' || timeSincePut > 7 * millisecondsInDay) {
+          window.caches.delete(scope);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    });
   }
 
   public destroy() {
-    eventTarget.removeEventListener(
-      'CORNERSTONE_LOCALFORAGE_WRITE_FAILURE',
-      this.handleLocalForageWriteError
-    );
     eventTarget.removeEventListener(
       'CORNERSTONE_CACHE_QUOTA_EXCEEDED_ERROR',
       this.handleQuotaExceededWriteError
